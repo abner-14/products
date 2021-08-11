fe52b6b89dc3739f59f460d777d980145adca3dc
abner-14 <abnerfunes5@gmail.com>
2021-08-11 08:49:48 -0600

    vamos

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 module.exports={
 
     mongodb:{
-        uri:'mongodb+srv://sharon-07:manaenportillo@cluster0.lgsnf.mongodb.net/Producto?retryWrites=true&w=majority'
+        uri:'mongodb://inventario:e1e1b9a80ec2f4db827f9d93dde44220@161.35.59.201:30017/inventario?retryWrites=true&w=majority'
 
     }
 
